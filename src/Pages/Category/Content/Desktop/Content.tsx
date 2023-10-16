485f0de89be7ce4211da55c463c4dc0782080619
EmilFerdinanA <emilakbar55@gmail.com>
2023-10-16 12:28:07 +0700

    fix 1

---
+++ src/Pages/Category/Content/Desktop/Content.tsx
@@ -0,0 +1,54 @@
+import { ButtonSeeProduct } from "@/components/ButtonSeeProduct";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface Headphone {
+  id: number;
+  image: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+  title: string;
+  description: string;
+}
+
+export const ContentDesktop = () => {
+  const [content, setContent] = useState<Headphone[]>([]);
+
+  const fetchCategory = async () => {
+    const response = await axios.get("src/data/Category/Category.json");
+    setContent(response.data);
+  };
+
+  useEffect(() => {
+    fetchCategory();
+  }, []);
+
+  return (
+    <section className="mt-16 mb-32 flex flex-col gap-32 px-6 sm:px-10">
+      {content?.map((e: Headphone, index: number) => (
+        <div className="flex  gap-6 justify-center items-center" key={e.id}>
+          {/* <div
+            className={`w-1/2 bg-tertiary bg-cover bg-center h-[35rem] ${e?.image.desktop} rounded-lg`}
+          /> */}
+          <div>
+            <img src={e?.image.desktop} />
+          </div>
+          <div className="w-1/2">
+            {index === 0 && (
+              <span className="text-primary tracking-[10px]">NEW PRODUCT</span>
+            )}
+            <span className="text-3xl sm:text-5xl sm:w-1/2 font-bold text-center">
+              {e?.title}
+            </span>
+            <p className="font-medium text-septenary text-opacity-50 text-center sm:w-3/4">
+              {e?.description}
+            </p>
+            <ButtonSeeProduct />
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
--- src/Pages/Category/Content/Mobile/Content.tsx
+++ src/Pages/Category/Content/Mobile/Content.tsx
@@ -1,5 +1,6 @@
 import { ButtonSeeProduct } from "@/components/ButtonSeeProduct";
-import { dataCategory } from "@/data/Category/Category";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 interface Headphone {
   id: number;
@@ -13,25 +14,37 @@ interface Headphone {
 }
 
 export const ContentMobile = () => {
+  const [content, setContent] = useState<Headphone[]>([]);
   const isMobile = window.innerWidth <= 640;
+
+  const fetchCategory = async () => {
+    const response = await axios.get("src/data/Category/Category.json");
+    setContent(response.data);
+  };
+
+  useEffect(() => {
+    fetchCategory();
+  }, []);
 
   return (
     <section className="mt-16 mb-32 flex flex-col gap-32 px-6 sm:px-10">
-      {dataCategory?.map((e: Headphone, index: number) => (
+      {content?.map((e: Headphone, index: number) => (
         <div
           className="flex flex-col gap-6 justify-center items-center"
           key={e.id}
         >
           <div
-            className={`bg-tertiary bg-cover h-80 ${
+            className={`bg-tertiary bg-cover bg-center h-80 sm:h-96 ${
               isMobile ? e?.image.mobile : e?.image.tablet
             } w-full rounded-lg`}
           />
           {index === 0 && (
             <span className="text-primary tracking-[10px]">NEW PRODUCT</span>
           )}
-          <span className="text-3xl font-bold text-center">{e?.title}</span>
-          <p className="font-medium text-septenary text-opacity-50 text-center">
+          <span className="text-3xl sm:text-5xl sm:w-1/2 font-bold text-center">
+            {e?.title}
+          </span>
+          <p className="font-medium text-septenary text-opacity-50 text-center sm:w-3/4">
             {e?.description}
           </p>
           <ButtonSeeProduct />
