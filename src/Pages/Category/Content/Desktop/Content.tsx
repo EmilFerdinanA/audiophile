e0f83b24b72e02f371e7b90e79dfccd8e222102b
EmilFerdinanA <emilakbar55@gmail.com>
2023-10-16 13:12:51 +0700

    fix 7

--- src/Pages/Category/Content/Desktop/Content.tsx
+++ src/Pages/Category/Content/Desktop/Content.tsx
@@ -1,7 +1,6 @@
-// import { ButtonSeeProduct } from "@/components/ButtonSeeProduct";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { data } from "@/data/Category/Category";
 
+// import { ButtonSeeProduct } from "@/components/ButtonSeeProduct"
 interface Headphone {
   id: number;
   image: {
@@ -14,21 +13,9 @@ interface Headphone {
 }
 
 export const ContentDesktop = () => {
-  const [content, setContent] = useState<Headphone[]>([]);
-
-  const fetchCategory = async () => {
-    const response = await axios.get("src/data/Category/Category.json");
-
-    setContent(response.data);
-  };
-
-  useEffect(() => {
-    fetchCategory();
-  }, []);
-
   return (
     <section className="mt-16 mb-32 flex flex-col gap-32 px-6 sm:px-10">
-      {content?.map((e: Headphone, index: number) => (
+      {data?.map((e: Headphone, index: number) => (
         <div key={index}>
           <img src={e?.image.desktop} />
         </div>
