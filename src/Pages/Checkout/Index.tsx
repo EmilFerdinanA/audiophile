7f614868de22333c8f97b88a5e3a6a81ab5416e5
EmilFerdinanA <emilakbar55@gmail.com>
2023-10-22 18:22:36 +0700

    add font family

--- src/Pages/Checkout/Index.tsx
+++ src/Pages/Checkout/Index.tsx
@@ -1,14 +1,16 @@
+import { CheckoutDesktop } from "./Dektop/Checkout";
+import { CheckoutMobile } from "./Mobile/Checkout";
 import { GoBack } from "@/components/GoBack";
-import { Checkout } from "./Checkout/Index";
 
-export const Checkouts = () => {
+export const Checkout = () => {
+  const isMobile = window.innerWidth <= 1280;
   return (
     <section className="bg-tertiary pb-36">
       <div className=" px-6 sm:px-10 xl:px-40 pt-4 mb-6 sm:pt-8 xl:pt-20 xl:mb-14">
         <GoBack />
       </div>
       <div>
-        <Checkout />
+        <div>{isMobile ? <CheckoutMobile /> : <CheckoutDesktop />}</div>;
       </div>
     </section>
   );
