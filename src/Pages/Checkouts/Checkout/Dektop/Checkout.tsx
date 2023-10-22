6c9c8993b7f5d6d1dd5317ee3187c11eed280b60
EmilFerdinanA <emilakbar55@gmail.com>
2023-10-22 17:00:25 +0700

    test modal

--- src/Pages/Checkouts/Checkout/Dektop/Checkout.tsx
+++ src/Pages/Checkouts/Checkout/Dektop/Checkout.tsx
@@ -11,154 +11,159 @@ export const CheckoutDesktop = () => {
     console.log(data);
   };
   return (
-    <div className="flex gap-8 mx-40">
-      <section className=" bg-senary rounded-lg px-12 py-14 w-2/3">
-        <h1 className="text-3xl font-bold mb-8">CHECKOUT</h1>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <section>
-            <h2 className="text-primary font-bold mb-4">BILLING DETAILS</h2>
-            <div className="flex gap-6 flex-wrap justify-between">
-              <Input
-                label="Name"
-                name="name"
-                register={register}
-                className="sm:w-[48%]"
-              />
-              <Input
-                className="sm:w-[48%]"
-                type="email"
-                label="Email Adress"
-                name="email"
-                register={register}
-              />
-              <Input
-                className="sm:w-[48%]"
-                type="number"
-                label="Phone Number"
-                name="phone_number"
-                register={register}
-              />
-            </div>
-          </section>
-
-          <section className="mt-8">
-            <h2 className="text-primary font-bold mb-4">SHIPPING INFO</h2>
-            <div className="flex gap-6 flex-wrap justify-between">
-              <Input
-                label="Your Address"
-                name="address"
-                register={register}
-                className="sm:w-full"
-              />
-              <Input
-                type="number"
-                label="ZIP Code"
-                name="zip_code"
-                register={register}
-                className="sm:w-[48%]"
-              />
-              <Input
-                label="City"
-                name="city"
-                register={register}
-                className="sm:w-[48%]"
-              />
-              <Input
-                label="Country"
-                name="country"
-                register={register}
-                className="sm:w-[48%]"
-              />
-            </div>
-          </section>
-
-          <section className="mt-8">
-            <h2 className="text-primary font-bold mb-4">PAYMENT DETAILS</h2>
-            <div className="flex justify-between">
-              <span className="text-xs font-bold">Payment Method</span>
-              <div className="flex flex-col gap-4 w-[48%]">
-                <Radio
-                  label="e-Money"
-                  name="payment"
+    <section>
+      <div className="flex gap-8 mx-40">
+        <section className=" bg-senary rounded-lg px-12 py-14 w-2/3">
+          <h1 className="text-3xl font-bold mb-8">CHECKOUT</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <section>
+              <h2 className="text-primary font-bold mb-4">BILLING DETAILS</h2>
+              <div className="flex gap-6 flex-wrap justify-between">
+                <Input
+                  label="Name"
+                  name="name"
                   register={register}
-                  value="e_money"
-                  checked={true}
+                  className="sm:w-[48%]"
                 />
-                <Radio
-                  label="Cash on Delivery"
-                  name="payment"
+                <Input
+                  className="sm:w-[48%]"
+                  type="email"
+                  label="Email Adress"
+                  name="email"
                   register={register}
-                  value="COD"
+                />
+                <Input
+                  className="sm:w-[48%]"
+                  type="number"
+                  label="Phone Number"
+                  name="phone_number"
+                  register={register}
                 />
               </div>
-            </div>
-            <div className="flex gap-6 mt-6 justify-between">
-              <Input
-                type="number"
-                label="e-Money Number"
-                name="e_money_number"
-                register={register}
-                className="w-[48%]"
-              />
-              <Input
-                type="number"
-                label="e-Money PIN"
-                name="e_money_pin"
-                register={register}
-                className="w-[48%]"
-              />
-            </div>
-          </section>
-        </form>
-      </section>
+            </section>
 
-      <section className="bg-senary rounded-lg px-8 py-8 w-4/12 h-full">
-        <h1 className="text-lg font-bold mb-8">SUMMARY</h1>
-        <div className="flex items-center justify-between">
-          <div className="flex items-center h-16 gap-4">
-            <img className="rounded-lg object-cover h-full" src={zx9} />
-            <div>
-              <h6 className="text-sm font-bold">XX99 MK II</h6>
-              <span className="text-sm font-bold text-septenary text-opacity-50">
-                $ 2,999
-              </span>
-            </div>
-          </div>
-          <span className="text-sm font-bold text-septenary text-opacity-50">
-            x1
-          </span>
-        </div>
+            <section className="mt-8">
+              <h2 className="text-primary font-bold mb-4">SHIPPING INFO</h2>
+              <div className="flex gap-6 flex-wrap justify-between">
+                <Input
+                  label="Your Address"
+                  name="address"
+                  register={register}
+                  className="sm:w-full"
+                />
+                <Input
+                  type="number"
+                  label="ZIP Code"
+                  name="zip_code"
+                  register={register}
+                  className="sm:w-[48%]"
+                />
+                <Input
+                  label="City"
+                  name="city"
+                  register={register}
+                  className="sm:w-[48%]"
+                />
+                <Input
+                  label="Country"
+                  name="country"
+                  register={register}
+                  className="sm:w-[48%]"
+                />
+              </div>
+            </section>
 
-        <div className="flex flex-col gap-2 mt-8">
-          <div className="flex justify-between">
-            <h6 className="text-base font-medium text-septenary text-opacity-50">
-              TOTAL
-            </h6>
-            <span className="text-lg font-bold">$ 5,396</span>
+            <section className="mt-8">
+              <h2 className="text-primary font-bold mb-4">PAYMENT DETAILS</h2>
+              <div className="flex justify-between">
+                <span className="text-xs font-bold">Payment Method</span>
+                <div className="flex flex-col gap-4 w-[48%]">
+                  <Radio
+                    label="e-Money"
+                    name="payment"
+                    register={register}
+                    value="e_money"
+                    checked={true}
+                  />
+                  <Radio
+                    label="Cash on Delivery"
+                    name="payment"
+                    register={register}
+                    value="COD"
+                  />
+                </div>
+              </div>
+              <div className="flex gap-6 mt-6 justify-between">
+                <Input
+                  type="number"
+                  label="e-Money Number"
+                  name="e_money_number"
+                  register={register}
+                  className="w-[48%]"
+                />
+                <Input
+                  type="number"
+                  label="e-Money PIN"
+                  name="e_money_pin"
+                  register={register}
+                  className="w-[48%]"
+                />
+              </div>
+            </section>
+          </form>
+        </section>
+
+        <section className="bg-senary rounded-lg px-8 py-8 w-4/12 h-full">
+          <h1 className="text-lg font-bold mb-8">SUMMARY</h1>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center h-16 gap-4">
+              <img className="rounded-lg object-cover h-full" src={zx9} />
+              <div>
+                <h6 className="text-sm font-bold">XX99 MK II</h6>
+                <span className="text-sm font-bold text-septenary text-opacity-50">
+                  $ 2,999
+                </span>
+              </div>
+            </div>
+            <span className="text-sm font-bold text-septenary text-opacity-50">
+              x1
+            </span>
           </div>
-          <div className="flex justify-between">
-            <h6 className="text-base font-medium text-septenary text-opacity-50">
-              SHIPPING
-            </h6>
-            <span className="text-lg font-bold">$ 50</span>
+
+          <div className="flex flex-col gap-2 mt-8">
+            <div className="flex justify-between">
+              <h6 className="text-base font-medium text-septenary text-opacity-50">
+                TOTAL
+              </h6>
+              <span className="text-lg font-bold">$ 5,396</span>
+            </div>
+            <div className="flex justify-between">
+              <h6 className="text-base font-medium text-septenary text-opacity-50">
+                SHIPPING
+              </h6>
+              <span className="text-lg font-bold">$ 50</span>
+            </div>
+            <div className="flex justify-between">
+              <h6 className="text-base font-medium text-septenary text-opacity-50">
+                VAT (INCLUDE)
+              </h6>
+              <span className="text-lg font-bold">$ 50</span>
+            </div>
+            <div className="flex justify-between mt-4">
+              <h6 className="text-base font-medium text-septenary text-opacity-50">
+                GRAND TOTAL
+              </h6>
+              <span className="text-primary text-lg font-bold">$ 5,446</span>
+            </div>
+            <button
+              onClick={handleSubmit(onSubmit)}
+              className="bg-primary h-12 text-senary font-bold mt-6"
+            >
+              CONTINUE & PAY
+            </button>
           </div>
-          <div className="flex justify-between">
-            <h6 className="text-base font-medium text-septenary text-opacity-50">
-              VAT (INCLUDE)
-            </h6>
-            <span className="text-lg font-bold">$ 50</span>
-          </div>
-          <div className="flex justify-between mt-4">
-            <h6 className="text-base font-medium text-septenary text-opacity-50">
-              GRAND TOTAL
-            </h6>
-            <span className="text-primary text-lg font-bold">$ 5,446</span>
-          </div>
-          <button className="bg-primary h-12 text-senary font-bold mt-6">
-            CONTINUE & PAY
-          </button>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </section>
   );
 };
---
+++ src/components/ModalOrder.tsx
@@ -0,0 +1,7 @@
+export const ModalOrder = () => {
+  return (
+    <div className="bg-black fixed rounded shadow-lg h-full w-full z-10">
+      <div></div>
+    </div>
+  );
+};
