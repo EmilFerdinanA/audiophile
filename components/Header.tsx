a8a4fe6781e8dd679f1725abcbd052ac2cc935db
EmilFerdinanA <emilakbar55@gmail.com>
2025-08-13 21:33:51 +0700

    fix: header navbar styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,31 +5,40 @@ const MENUS = ["HOME", "HEADPHONES", "SPEAKERS", "EARPHONES"];
 
 export const Header = () => {
   return (
-    <div className="sticky bg-[#191919] flex justify-center h-24 top-0 w-full z-50 px-6 md:px-10 lg:px-40">
-      <div className="containers !justify-between w-full border-b-1 border-white max-w-[1440px]">
-        <img
-          className="md:absolute lg:hidden"
-          src="/assets/icon-hamburger.svg"
-          alt="hamburger"
-        />
+    <div className="sticky bg-[#191919] flex justify-center h-24 top-0 z-50">
+      {/* Container */}
+      <div className="max-w-[1440px] base-p w-full">
+        {/* Content */}
+        <div className="containers !justify-between border-b-1 border-white/20 h-full">
+          <img
+            className="md:absolute lg:hidden"
+            src="/assets/icon-hamburger.svg"
+            alt="hamburger"
+          />
 
-        <Link href="/" className="relative md:left-14 lg:left-0 cursor-pointer">
-          <img src="/assets/logo.svg" alt="logo" />
-        </Link>
+          <Link
+            href="/"
+            className="relative md:left-14 lg:left-0 cursor-pointer"
+          >
+            <img src="/assets/logo.svg" alt="logo" />
+          </Link>
 
-        <ul className={"hidden lg:flex gap-8 subtitle text-white font-bold"}>
-          {MENUS.map((menu, index) => (
-            <li key={index} className="hover:text-primary cursor-pointer">
-              <Link
-                href={menu === "HOME" ? "/" : `/category/${menu.toLowerCase()}`}
-              >
-                {menu}
-              </Link>
-            </li>
-          ))}
-        </ul>
+          <ul className={"hidden lg:flex gap-8 subtitle text-white font-bold"}>
+            {MENUS.map((menu, index) => (
+              <li key={index} className="hover:text-[#D87D4A] cursor-pointer">
+                <Link
+                  href={
+                    menu === "HOME" ? "/" : `/category/${menu.toLowerCase()}`
+                  }
+                >
+                  {menu}
+                </Link>
+              </li>
+            ))}
+          </ul>
 
-        <img src="/assets/icon-cart.svg" alt="logo" />
+          <img src="/assets/icon-cart.svg" alt="logo" />
+        </div>
       </div>
     </div>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import { SeeProductButton } from "./SeeProduct";
 export const Hero = () => {
   return (
     <div className="containers bg-[#191919]">
-      <div className="background-hero bg-cover bg-center h-[39.5rem] max-w-[1440px] w-full">
+      <div className="background-hero h-[39.5rem] max-w-[1440px] w-full">
         <div className="containers lg:!items-start text-white flex-col gap-6 text-center lg:text-justify w-full md:w-[23.688rem]">
           <span className="overlines">NEW PRODUCT</span>
           <h1 className="!text-4xl md:!text-[3.5rem]">
