8b8a877e2407417a4ddb20bf434b49af6c1741e9
EmilFerdinanA <emilakbar55@gmail.com>
2025-08-14 00:32:27 +0700

    hero desktop

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,18 +2,18 @@ import { SeeProductButton } from "./SeeProduct";
 
 export const Hero = () => {
   return (
-    <div className="bg-[#191919]">
+    <div className="bg-[#191919] containers">
       {/* Container */}
-      <div className="containers background-hero h-[36rem] md:h-[43rem] max-w-[1440px] base-p">
+      <div className="containers background-hero h-[36rem] md:h-[45rem] max-w-[1440px] base-p w-full lg:!justify-start">
         {/* Content */}
-        <div className="containers flex-col text-white text-center gap-4 md:w-96">
+        <div className="containers lg:!items-start flex-col text-white text-center lg:text-left gap-4 md:w-96">
           <span className="opacity-50 tracking-[10px] text-sm">
             NEW PRODUCT
           </span>
           <h1 className="text-[2.25rem] md:text-[3.5rem] font-bold leading-[2.5rem] md:leading-[3.5rem]">
             XX99 MARK II HEADPHONES
           </h1>
-          <span className="opacity-75 mb-4 md:text-sm p-4 md:p-5">
+          <span className="opacity-75 mb-4 md:text-sm px-4 md:px-5 lg:p-0 lg:pr-10">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </span>
