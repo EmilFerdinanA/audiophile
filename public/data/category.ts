36587c78e3bfe50a9fe772c7d6d866b94763ed0f
EmilFerdinanA <emilakbar55@gmail.com>
2025-09-19 00:14:25 +0700

    fix: data for category

--- public/data/category.ts
+++ public/data/category.ts
@@ -1,14 +1,6 @@
 export const dataCategory = [
   {
     category: "headphones",
-    image: {
-      mobile: "assets/category-headphones/mobile/image-xx99-mark-two.jpg",
-      tablet: "assets/category-headphones/tablet/image-xx99-mark-two.jpg",
-      desktop: "assets/category-headphones/desktop/image-xx99-mark-two.jpg",
-    },
-  },
-  {
-    category: "earphones",
     data: [
       {
         id: 1,
@@ -16,27 +8,43 @@ export const dataCategory = [
         description:
           "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
         image: {
-          mobile: "assets/category-earphones/mobile/image-yx1-earphones.jpg",
-          tablet: "assets/category-earphones/tablet/image-yx1-earphones.jpg",
-          desktop: "assets/category-earphones/desktop/image-yx1-earphones.jpg",
+          mobile: "assets/category-headphones/mobile/image-xx99-mark-two.jpg",
+          tablet: "assets/category-headphones/tablet/image-xx99-mark-two.jpg",
+          desktop: "assets/category-headphones/desktop/image-xx99-mark-two.jpg",
         },
       },
       {
         id: 2,
-        title: "XX99 Mark II Headphones",
+        title: "XX99 Mark I Headphones",
         description:
-          "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+          "As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.",
         image: {
-          mobile: "assets/category-earphones/mobile/image-yx1-earphones.jpg",
-          tablet: "assets/category-earphones/tablet/image-yx1-earphones.jpg",
-          desktop: "assets/category-earphones/desktop/image-yx1-earphones.jpg",
+          mobile: "assets/category-headphones/mobile/image-xx99-mark-one.jpg",
+          tablet: "assets/category-headphones/tablet/image-xx99-mark-one.jpg",
+          desktop: "assets/category-headphones/desktop/image-xx99-mark-one.jpg",
         },
       },
       {
         id: 3,
-        title: "XX99 Mark II Headphones",
+        title: "XX59 Headphones",
         description:
-          "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.",
+          "Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move.",
+        image: {
+          mobile: "assets/category-headphones/mobile/image-xx59.jpg",
+          tablet: "assets/category-headphones/tablet/image-xx59.jpg",
+          desktop: "assets/category-headphones/desktop/image-xx59.jpg",
+        },
+      },
+    ],
+  },
+  {
+    category: "earphones",
+    data: [
+      {
+        id: 1,
+        title: "YX1 WIRELESS EARPHONES",
+        description:
+          "Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.",
         image: {
           mobile: "assets/category-earphones/mobile/image-yx1-earphones.jpg",
           tablet: "assets/category-earphones/tablet/image-yx1-earphones.jpg",
@@ -44,18 +52,32 @@ export const dataCategory = [
         },
       },
     ],
-    image: {
-      mobile: "assets/category-earphones/mobile/image-yx1-earphones.jpg",
-      tablet: "assets/category-earphones/tablet/image-yx1-earphones.jpg",
-      desktop: "assets/category-earphones/desktop/image-yx1-earphones.jpg",
-    },
   },
   {
     category: "speakers",
-    image: {
-      mobile: "assets/category-speakers/mobile/image-zx9.jpg",
-      tablet: "assets/category-speakers/tablet/image-zx9.jpg",
-      desktop: "assets/category-speakers/desktop/image-zx9.jpg",
-    },
+    data: [
+      {
+        id: 1,
+        title: "ZX9 SPEAKER",
+        description:
+          "Upgrade your sound system with the all new ZX9 active speaker. It’s a bookshelf speaker system that offers truly wireless connectivity -- creating new possibilities for more pleasing and practical audio setups.",
+        image: {
+          mobile: "assets/category-speakers/mobile/image-zx9.jpg",
+          tablet: "assets/category-speakers/tablet/image-zx9.jpg",
+          desktop: "assets/category-speakers/desktop/image-zx9.jpg",
+        },
+      },
+      {
+        id: 2,
+        title: "ZX7 SPEAKER",
+        description:
+          "Stream high quality sound wirelessly with minimal loss. The ZX7 bookshelf speaker uses high-end audiophile components that represents the top of the line powered speakers for home or studio use.",
+        image: {
+          mobile: "assets/category-speakers/mobile/image-zx7.jpg",
+          tablet: "assets/category-speakers/tablet/image-zx7.jpg",
+          desktop: "assets/category-speakers/desktop/image-zx7.jpg",
+        },
+      },
+    ],
   },
 ];
