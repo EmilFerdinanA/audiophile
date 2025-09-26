334edbe2ce4d567ec0439ccd04a668a5fe2fd483
EmilFerdinanA <emilakbar55@gmail.com>
2025-09-26 23:13:47 +0700

    improve: change render image from bg to picture yx1

--- components/YX1.tsx
+++ components/YX1.tsx
@@ -2,11 +2,25 @@ import { SeeProductButton } from "./SeeProduct";
 
 export default function YX1() {
   return (
-    <section className="containers !flex-col md:!flex-row gap-6 md:gap-3 lg:gap-8 mb-32">
-      <div className="background-yx1 rounded-lg h-52 md:h-80 w-full md:w-1/2" />
+    <section className="grid grid-cols-2 gap-6 md:gap-3 lg:gap-8 mb-32">
+      <picture className="col-span-2 md:col-span-1">
+        <source
+          media="(min-width:1024px)"
+          srcSet={"/assets/home/desktop/image-earphones-yx1.jpg"}
+        />
+        <source
+          media="(min-width:768px)"
+          srcSet={"/assets/home/tablet/image-earphones-yx1.jpg"}
+        />
+        <img
+          className="object-center object-cover rounded-lg h-52 md:h-80 w-full"
+          src={"/assets/home/mobile/image-earphones-yx1.jpg"}
+          alt="YX1"
+        />
+      </picture>
 
-      <div className="bg-[#F1F1F1] flex flex-col items-start justify-center gap-8 rounded-lg px-6 md:px-16 lg:px-24 h-52 md:h-80 w-full md:w-1/2">
-        <h1 className="font-bold text-3xl">YX1 EARPHONES</h1>
+      <div className="col-span-2 md:col-span-1 bg-[#F1F1F1] rounded-lg grid place-content-center justify-start h-52 md:h-80 p-6 md:p-10 lg:p-24">
+        <h1 className="font-bold text-3xl mb-8">YX1 EARPHONES</h1>
         <SeeProductButton variant="secondary" />
       </div>
     </section>
