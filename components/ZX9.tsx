c84f33e94a6ad19df95cd66234fa209e39ef9e04
EmilFerdinanA <emilakbar55@gmail.com>
2025-09-27 12:16:35 +0700

    fix: zx9 bg position can't read on production

--- components/ZX9.tsx
+++ components/ZX9.tsx
@@ -4,21 +4,23 @@ import ZX9Image from "@/public/assets/home/desktop/image-speaker-zx9.png";
 
 export default function ZX9() {
   return (
-    <div className="container-zx9 background-zx9 h-zx9 mt-32 py-14 lg:py-0 bg-primary rounded-lg">
-      <Image
-        src={ZX9Image}
-        alt={"zx9"}
-        className="w-44 md:w-56 lg:w-96 mb-10 lg:mb-0 lg:left-16 lg:relative"
-      />
+    <div className="background-zx9 mt-32 py-14 md:py-16 lg:py-0 lg:pt-24 px-6 md:px-44 lg:px-24 rounded-lg grid lg:place-content-end grid-cols-2 gap-8 md:gap-16">
+      <div className="col-span-2 lg:col-span-1 justify-self-center lg:justify-self-end">
+        <Image src={ZX9Image} alt={"zx9"} className="w-44 md:w-56 lg:w-96" />
+      </div>
 
-      <div className="containers !flex-col lg:!items-start gap-6 px-6 md:px-40 lg:px-0 lg:pl-56 lg:h-full lg:w-1/2 text-white text-center lg:text-left">
-        <h1 className="font-bold text-4xl md:text-6xl lg:text-5xl px-13 lg:px-0 lg:pr-5">
-          ZX9 SPEAKER
+      <div className="text-white text-center lg:text-left col-span-2 lg:col-span-1 grid place-items-center lg:place-items-start lg:place-content-start gap-6 lg:ml-20">
+        <h1 className="font-bold text-4xl md:text-6xl lg:text-5xl leading-10 md:leading-14">
+          ZX9
+          <br />
+          SPEAKER
         </h1>
-        <p className="text-[15px]/6 opacity-75 md:mb-4 md:px-6 lg:px-0">
+
+        <p className="text-[15px]/6 opacity-75 md:mb-4">
           Upgrade to premium speakers that are phenomenally built to deliver
           truly remarkable sound.
         </p>
+
         <SeeProductButton variant="tertiary" />
       </div>
     </div>
